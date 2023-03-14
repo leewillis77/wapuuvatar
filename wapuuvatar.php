6e5bc3f069c96fff07613f90da32fd899129c3a3
Lee Willis <lee@ademti-software.co.uk>
2023-03-14 13:22:57 +0000

    Bump (v3.0.3)

--- wapuuvatar.php
+++ wapuuvatar.php
@@ -5,7 +5,7 @@ Description: Use Wapuus for your user avatars.
 Plugin URI: http://www.leewillis.co.uk
 Author: Lee Willis
 Author URI: http://www.leewillis.co.uk
-Version: 3.0.2
+Version: 3.0.3
 License: GPL2
 Text Domain: wapuuvatar
 */
