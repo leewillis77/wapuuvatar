a850336c33bf7d20b6d9e05855e11a3312daa469
Lee Willis <lee@ademti-software.co.uk>
2023-03-14 13:23:16 +0000

    Tagging for release

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 # A modification of Dean Clatworthy's deploy script as found here: https://github.com/deanc/wordpress-plugin-git-svn
 # The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
